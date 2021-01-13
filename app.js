926a8a0cb6605e2f9c79df1436e20ba177b32d85
Acool4ik <byshkov.oleg@yandex.ru>
2021-01-13 21:23:52 +0300

    test4

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(favicon(__dirname + 'frontend/public/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'frontend', 'build')))
 
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 })
 
