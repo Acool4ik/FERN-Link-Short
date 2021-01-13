4905e304e8ce9a4e27ad7ec313c47b3fb688b636
Acool4ik <byshkov.oleg@yandex.ru>
2021-01-13 20:21:08 +0300

    make it better

--- app.js
+++ app.js
@@ -21,11 +21,10 @@ const PORT = config.get('port') || 5000
 if(process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
-
 
 app.use(express.urlencoded({ extended: false })) // for parse body and json
 app.use(express.json())
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 
+
 router.get('/api/image/:id/download', async (req, res) => {
     const { params: { id: img }} = req
 
@@ -15,10 +16,14 @@ router.get('/api/clicks/:id', async (req, res) => {
     const { params: { id}} = req
 
     try {
-        
-
         res.json({ data: Date.now() })
     } catch(err) { return res.status(400) }
+})
+
+router.get('/api/linkitem/:id', async (req, res) => {
+    
+
+    res.json({error: false})
 })
 
 module.exports = router
